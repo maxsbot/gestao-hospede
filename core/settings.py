819a123dfe2ffbd1d9e498be7273d8969ee58110
maxsbot <maaxvinicius@gmail.com>
2024-12-26 14:45:49 -0300

    ajustes

--- core/settings.py
+++ core/settings.py
@@ -119,9 +119,14 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # Added leading slash
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# Add this for development
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / 'static']
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Whitenoise configuration
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # comentado para desenvolvimento local

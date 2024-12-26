9b2a55c84b0ac1a53bb9c0eec561f7b58e0e645b
maxsbot <maaxvinicius@gmail.com>
2024-12-26 14:37:47 -0300

    ajustes

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,10 @@ SECRET_KEY = config('SECRET_KEY', default='your-secret-key-here')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
+
+
 
 # Application definition
 

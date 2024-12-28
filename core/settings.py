0b142db3a6c795fb3cb240119a7b80973623d7f7
maxsbot <maaxvinicius@gmail.com>
2024-12-28 14:55:09 -0300

    ajuste no csrf para receber adequadamente

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,11 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
 
-CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='*', cast=Csv())
+# CSRF settings
+CSRF_TRUSTED_ORIGINS = []
+csrf_origins = config('CSRF_TRUSTED_ORIGINS', cast=Csv())
+if csrf_origins:
+    CSRF_TRUSTED_ORIGINS = [origin.strip() for origin in csrf_origins]
 
 # Application definition
 

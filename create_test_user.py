2e6ac810b71de0f77e6884cdf19a01a917e8c4c3
maxsbot <maaxvinicius@gmail.com>
2024-12-28 12:53:03 -0300

    ajustes contas de teste e requirements

--- create_test_user.py
+++ create_test_user.py
@@ -8,18 +8,36 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-# Criar um usuário de teste
-username = '+5579998830295'  # Número de telefone com código do país
-email = 'teste@example.com'
+# Lista de usuários de teste
+test_users = [
+    {
+        'username': '+5579998830295',
+        'email': 'teste@example.com',
+        'password': 'senha123'
+    },
+    {
+        'username': '+557999002792',
+        'email': 'teste2@example.com',
+        'password': 'anac9465'
+    },
+    {
+        'username': '+5583996617860',
+        'email': 'teste3@example.com',
+        'password': '1303babi'
+    }
+]
 
-if not User.objects.filter(username=username).exists():
-    user = User.objects.create_user(
-        username=username,
-        email=email,
-        password='senha123'  # Senha para testes
-    )
-    print(f'Usuário criado com sucesso!')
-    print(f'Username (telefone): {username}')
-    print(f'Senha: senha123')
-else:
-    print(f'Usuário {username} já existe!')
+# Criar usuários de teste
+for user_data in test_users:
+    username = user_data['username']
+    if not User.objects.filter(username=username).exists():
+        user = User.objects.create_user(
+            username=username,
+            email=user_data['email'],
+            password=user_data['password']
+        )
+        print(f'Usuário criado com sucesso!')
+        print(f'Username (telefone): {username}')
+        print(f'Senha: {user_data["password"]}')
+    else:
+        print(f'Usuário {username} já existe!')
